56fd9a260081fca0af5ba00a30641e979994fcb3
Russell161 <rleggjunior@gmail.com>
2025-04-26 11:54:58 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Please insert your first name");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("Please insert your")
+        Console.WriteLine($"Your name is {firstname}");
+
     }
 }

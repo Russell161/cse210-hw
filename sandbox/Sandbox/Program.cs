6f39f43d89541e4e52da14ef5641a1886090eaed
Russell161 <rleggjunior@gmail.com>
2025-04-29 09:38:06 -0600

    hw

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -14,51 +14,65 @@
 //     }
 // }
 
-using System;
-// using System.Configuration.Assemblies;
-// using System.Runtime.CompilerServices;
+// using System;
 // using System.Security.Cryptography.X509Certificates;
+// // using System.Configuration.Assemblies;
+// // using System.Runtime.CompilerServices;
+// // using System.Security.Cryptography.X509Certificates;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("What is your grade percentage? ");
-        string grade = Console.ReadLine();
-        int percent = int.Parse(grade);
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("What is your grade percentage? ");
+//         string grade = Console.ReadLine();
+//         int percent = int.Parse(grade);
 
-        string letter = "";
+//         string letter = "";
         
-        if (percent >= 90)
-        {
-            letter = "A";
-        }
-        else if (percent >= 80)
-        {
-            letter = "B";
-        }
-        else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
+//         if (percent >= 90)
+//         {
+//             letter = "A";
+//         }
+//         else if (percent >= 80)
+//         {
+//             letter = "B";
+//         }
+//         else if (percent >= 70)
+//         {
+//             letter = "C";
+//         }
+//         else if (percent >= 60)
+//         {
+//             letter = "D";
+//         }
+//         else
+//         {
+//             letter = "F";
+//         }
 
-        Console.WriteLine($"Your grade is: {letter}");
+//         Console.WriteLine($"Your grade is: {letter}");
 
-       if (percent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better luck next time!");
-        }
-    }
-}    
+//        if (percent >= 70)
+//         {
+//             Console.WriteLine("You passed!");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Better luck next time!");
+//         }
+//     }
+// }    
+
+int x = 10;
+if (x == 10)
+{
+    Console.WriteLine("x is 10");
+}
+
+for(int i = 0; i < x; i++)
+
+
+{
+    Console.WriteLine($"Bob is Cool: {i}");
+}
